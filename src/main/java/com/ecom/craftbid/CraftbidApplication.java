d4bce114dc201c1c856de2beb0eac447980d276d
Marek Szulc <235991@edu.p.lodz.pl>
2023-03-15 21:39:45 +0100

    Initial commit

---
+++ src/main/java/com/ecom/craftbid/CraftbidApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.craftbid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftbidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CraftbidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecom/craftbid/CraftbidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.craftbid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftbidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
