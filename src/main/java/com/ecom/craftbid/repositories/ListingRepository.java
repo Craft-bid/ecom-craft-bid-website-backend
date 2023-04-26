08c634199acf4ebb11f9571bc7db7aec59800a83
Marek Szulc <235991@edu.p.lodz.pl>
2023-04-26 11:00:15 +0200

    Add basic listing queries to the listing repo

--- src/main/java/com/ecom/craftbid/repositories/ListingRepository.java
+++ src/main/java/com/ecom/craftbid/repositories/ListingRepository.java
@@ -3,6 +3,20 @@ package com.ecom.craftbid.repositories;
 import com.ecom.craftbid.entities.listing.Listing;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ListingRepository extends JpaRepository<Listing, Long> {
+    List<Listing> findByEndedFalseOrderByExpirationDateAsc();
+    List<Listing> findByEndedFalseOrderByExpirationDateDesc();
+    List<Listing> findByEndedTrueOrderByExpirationDateAsc();
+    List<Listing> findByEndedTrueOrderByExpirationDateDesc();
+
+    List<Listing> findByEndedFalse();
+    List<Listing> findByEndedTrue();
+
+    List<Listing> findByAdvertiserId(long id);
+    List<Listing> findByWinnerId(long id);
+    List<Listing> findByBidsId(long id);
+    List<Listing> findByTitleContaining(String title);
 
 }
