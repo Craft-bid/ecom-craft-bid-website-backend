3c2587454ca89fc4d72c39533486c7e4b1f7f2fd
Marek Szulc <235991@edu.p.lodz.pl>
2023-04-26 10:43:35 +0200

    Add test checking propre listing and tag creation

--- src/test/java/com/ecom/craftbid/ListingCRUDTests.java
+++ src/test/java/com/ecom/craftbid/ListingCRUDTests.java
@@ -32,41 +32,42 @@ public class ListingCRUDTests {
     }
 
     void createSampleDatabase() {
-//        Listing car3DListing = Listing.builder()
-//                .title("car 3d printing")
-//                .description("I will 3d print you a car")
-//                .ended(false)
-//                .creationDate(new java.util.Date(System.currentTimeMillis()))
-//                .expirationDate(new java.util.Date(System.currentTimeMillis() + 7 * 24 * 60 * 60 * 1000))
-//                .build();
-//        listingRepository.save(car3DListing);
-//
-//        Tag car3DTag = Tag.builder().name("3d printing").build();
-//        car3DTag.setListings((List<Listing>) new HashSet<>(Collections.singletonList(car3DListing)));
-//        tagRepository.save(car3DTag);
-//
-//        car3DListing.setTags((Set<Tag>) new HashSet<>(Collections.singletonList(car3DTag)));
-//        listingRepository.save(car3DListing);
+        Listing car3DListing = Listing.builder()
+                .title("car 3d printing")
+                .description("I will 3d print you a car")
+                .ended(false)
+                .creationDate(new java.util.Date(System.currentTimeMillis()))
+                .expirationDate(new java.util.Date(System.currentTimeMillis() + 7 * 24 * 60 * 60 * 1000))
+                .photos(new ArrayList<>(Collections.singletonList("https://www.google.com")))
+                .build();
+        listingRepository.save(car3DListing);
+
+        Tag car3DTag = Tag.builder().name("3d printing").build();
+        List<Listing> list = new ArrayList<>(Collections.singletonList(car3DListing));
+        car3DTag.setListings(list);
+        tagRepository.save(car3DTag);
+
+        car3DListing.setTags((Set<Tag>) new HashSet<>(Collections.singletonList(car3DTag)));
+        listingRepository.save(car3DListing);
 
     }
 
     void cleanup() {
-        tagRepository.deleteAll();
         listingRepository.deleteAll();
+        tagRepository.deleteAll();
     }
 
 
+    @Test
+    void checkListingWithTagCreation() {
+        assert(listingRepository.count() == 1);
+        assert(tagRepository.count() == 1);
 
-//    @Test
-//    void checkListingWithTagCreation() {
-//        assert(listingRepository.count() == 1);
-//        assert(tagRepository.count() == 1);
-//
-//        Listing card3DListing = listingRepository.findAll().get(0);
-//        Tag car3DTag = tagRepository.findAll().get(0);
-//        log.info("Listing: " + card3DListing);
-//        log.info("Tag: " + car3DTag);
-//    }
+        Listing card3DListing = listingRepository.findAll().get(0);
+        Tag car3DTag = tagRepository.findAll().get(0);
+        log.info("Listing: " + card3DListing.getTitle());
+        log.info("Tag: " + car3DTag.getName());
+    }
 
 
 }
