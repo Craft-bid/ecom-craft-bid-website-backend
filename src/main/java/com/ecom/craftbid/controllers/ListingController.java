914380258b59c48d98fc459eca9f1c6c8cb8963c
Marek Szulc <235991@edu.p.lodz.pl>
2023-05-11 18:12:08 +0200

    Add basic operations to listing controller

--- src/main/java/com/ecom/craftbid/controllers/ListingController.java
+++ src/main/java/com/ecom/craftbid/controllers/ListingController.java
@@ -3,9 +3,9 @@ package com.ecom.craftbid.controllers;
 import com.ecom.craftbid.entities.listing.Listing;
 import com.ecom.craftbid.repositories.ListingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,4 +20,27 @@ public class ListingController {
     public List<Listing> getAllListings() {
         return listingRepository.findAll();
     }
+
+    @PostMapping
+    public Listing createListing(@RequestBody Listing listing) {
+        return listingRepository.save(listing);
+    }
+
+    @PutMapping("/{id}")
+    public Listing updateListing(@PathVariable long id, @RequestBody Listing updatedListing) throws ChangeSetPersister.NotFoundException {
+        Listing existingListing = listingRepository.findById(id)
+                .orElseThrow(ChangeSetPersister.NotFoundException::new);
+
+        existingListing.setTitle(updatedListing.getTitle());
+        existingListing.setDescription(updatedListing.getDescription());
+
+        return listingRepository.save(existingListing);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteListing(@PathVariable long id) {
+        listingRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
+
