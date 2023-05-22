2aff4b26dd5ff909ab7c41683418b37c75f9693b
Marek Szulc <62565929+shaxi1@users.noreply.github.com>
2023-05-22 21:06:28 +0200

    Add basic crud for bids (#9)

--- src/main/java/com/ecom/craftbid/controllers/BidController.java
+++ src/main/java/com/ecom/craftbid/controllers/BidController.java
@@ -1,2 +1,47 @@
-package com.ecom.craftbid.controllers;public class BidController {
+package com.ecom.craftbid.controllers;
+
+
+import com.ecom.craftbid.entities.listing.Bid;
+import com.ecom.craftbid.repositories.BidRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/bids")
+public class BidController {
+
+    @Autowired
+    private BidRepository bidRepository;
+
+    @GetMapping
+    public List<Bid> getAllBids() {
+        return bidRepository.findAll();
+    }
+
+    @PostMapping
+    public Bid createBid(Bid bid) {
+        return bidRepository.save(bid);
+    }
+
+    @PutMapping("/{id}")
+    public Bid updateBid(@PathVariable long id, @RequestBody Bid updatedBid) throws ChangeSetPersister.NotFoundException {
+        Bid existingBid = bidRepository.findById(id).orElseThrow(ChangeSetPersister.NotFoundException::new);
+
+        existingBid.setPrice(updatedBid.getPrice());
+        existingBid.setDescription(updatedBid.getDescription());
+        existingBid.setCreationDate(updatedBid.getCreationDate());
+        existingBid.setDaysToDeliver(updatedBid.getDaysToDeliver());
+        existingBid.setBidder(updatedBid.getBidder());
+        existingBid.setListing(updatedBid.getListing());
+
+        return bidRepository.save(existingBid);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBid(@PathVariable long id) {
+        bidRepository.deleteById(id);
+    }
 }
--- src/main/java/com/ecom/craftbid/repositories/BidRepository.java
+++ src/main/java/com/ecom/craftbid/repositories/BidRepository.java
@@ -1,2 +1,8 @@
-package com.ecom.craftbid.repositories;public class BidRepository {
+package com.ecom.craftbid.repositories;
+
+import com.ecom.craftbid.entities.listing.Bid;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BidRepository extends JpaRepository<Bid, Long> {
+    void deleteById(long id);
 }
