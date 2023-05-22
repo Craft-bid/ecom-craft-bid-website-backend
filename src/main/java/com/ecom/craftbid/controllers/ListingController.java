9f4744311b22b77c9c6c96fc598a3fd0ae99f089
Marek Szulc <62565929+shaxi1@users.noreply.github.com>
2023-05-22 20:55:24 +0200

    Update listing contoller (#7)
    * Add a way to assign tags to the listing
    
    * Add a way to assign bids to the listing
    
    * Add a way to set a winner for the listing
    
    * Add a way to get listings by the expiration date
    
    * Add a way to get listings by title containg a specific string

--- src/main/java/com/ecom/craftbid/controllers/ListingController.java
+++ src/main/java/com/ecom/craftbid/controllers/ListingController.java
@@ -1,13 +1,21 @@
 package com.ecom.craftbid.controllers;
 
+import com.ecom.craftbid.entities.listing.Bid;
 import com.ecom.craftbid.entities.listing.Listing;
+import com.ecom.craftbid.entities.listing.Tag;
+import com.ecom.craftbid.entities.user.User;
 import com.ecom.craftbid.repositories.ListingRepository;
+import com.ecom.craftbid.repositories.TagRepository;
+import com.ecom.craftbid.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/listings")
@@ -15,6 +23,12 @@ public class ListingController {
 
     @Autowired
     private ListingRepository listingRepository;
+
+    @Autowired
+    private TagRepository tagRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping
     public List<Listing> getAllListings() {
@@ -42,5 +56,107 @@ public class ListingController {
         listingRepository.deleteById(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PostMapping("/{listingId}/tags")
+    public Listing addTagsToListing(@PathVariable long listingId, @RequestBody List<Long> tagIds) throws ChangeSetPersister.NotFoundException {
+        Listing listing = listingRepository.findById(listingId)
+                .orElseThrow(ChangeSetPersister.NotFoundException::new);
+
+        Set<Tag> tags = new HashSet<>(tagRepository.findAllById(tagIds));
+
+        listing.getTags().addAll(tags);
+
+        return listingRepository.save(listing);
+    }
+
+    @DeleteMapping("/{listingId}/tags/{tagId}")
+    public Listing removeTagFromListing(@PathVariable long listingId, @PathVariable long tagId) throws ChangeSetPersister.NotFoundException {
+        Listing listing = listingRepository.findById(listingId)
+                .orElseThrow(ChangeSetPersister.NotFoundException::new);
+
+        Tag tag = tagRepository.findById(tagId)
+                .orElseThrow(ChangeSetPersister.NotFoundException::new);
+
+        listing.getTags().remove(tag);
+
+        return listingRepository.save(listing);
+    }
+
+    @PostMapping("/{listingId}/photos")
+    public Listing addPhotosToListing(@PathVariable long listingId, @RequestBody List<String> photos) throws ChangeSetPersister.NotFoundException {
+        Listing listing = listingRepository.findById(listingId)
+                .orElseThrow(ChangeSetPersister.NotFoundException::new);
+
+        listing.getPhotos().addAll(photos);
+
+        return listingRepository.save(listing);
+    }
+
+    @DeleteMapping("/{listingId}/photos/{photo}")
+    public Listing removePhotoFromListing(@PathVariable long listingId, @PathVariable String photo) throws ChangeSetPersister.NotFoundException {
+        Listing listing = listingRepository.findById(listingId)
+                .orElseThrow(ChangeSetPersister.NotFoundException::new);
+
+        listing.getPhotos().remove(photo);
+
+        return listingRepository.save(listing);
+    }
+
+    @PostMapping("/{listingId}/bids")
+    public Listing addBidToListing(@PathVariable long listingId, @RequestBody Bid bid) throws ChangeSetPersister.NotFoundException {
+        Listing listing = listingRepository.findById(listingId)
+                .orElseThrow(ChangeSetPersister.NotFoundException::new);
+
+        bid.setListing(listing);
+        listing.getBids().add(bid);
+
+        return listingRepository.save(listing);
+    }
+
+    @DeleteMapping("/{listingId}/bids/{bidId}")
+    public Listing removeBidFromListing(@PathVariable long listingId, @PathVariable long bidId) throws ChangeSetPersister.NotFoundException {
+        Listing listing = listingRepository.findById(listingId)
+                .orElseThrow(ChangeSetPersister.NotFoundException::new);
+
+        Bid bidToRemove = listing.getBids().stream()
+                .filter(bid -> bid.getId() == bidId)
+                .findFirst()
+                .orElseThrow(ChangeSetPersister.NotFoundException::new);
+
+        listing.getBids().remove(bidToRemove);
+
+        return listingRepository.save(listing);
+    }
+
+    @PutMapping("/{listingId}/winner/{userId}")
+    public Listing setWinnerForListing(@PathVariable long listingId, @PathVariable long userId) throws ChangeSetPersister.NotFoundException {
+        Listing listing = listingRepository.findById(listingId)
+                .orElseThrow(ChangeSetPersister.NotFoundException::new);
+
+        User winner = userRepository.findById(userId)
+                .orElseThrow(ChangeSetPersister.NotFoundException::new);
+
+
+        listing.setWinner(winner);
+
+        return listingRepository.save(listing);
+    }
+
+    @GetMapping("/active-listings")
+    public List<Listing> getActiveListingsSortedByExpirationDate() {
+        return listingRepository.findByEndedFalseOrderByExpirationDateDesc();
+    }
+
+    @GetMapping("/ended-listings")
+    public List<Listing> getEndedListingsSortedByExpirationDate() {
+        return listingRepository.findByEndedTrueOrderByExpirationDateDesc();
+    }
+
+    @GetMapping("/listings-by-title")
+    public List<Listing> getListingsByTitle(@RequestParam String title) {
+        return listingRepository.findByTitleContaining(title);
+    }
+
+
 }
 
--- src/main/java/com/ecom/craftbid/repositories/TagRepository.java
+++ src/main/java/com/ecom/craftbid/repositories/TagRepository.java
@@ -1,7 +1,11 @@
 package com.ecom.craftbid.repositories;
 
 import com.ecom.craftbid.entities.listing.Tag;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface TagRepository extends JpaRepository<Tag, Long> {
+
 }
