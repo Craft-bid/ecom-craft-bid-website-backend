b01ee6d8f1116c62733750940415e4af7befdb26
Maciej Iwan <64867638+MaciejIwan@users.noreply.github.com>
2023-06-10 17:10:34 +0200

    fix && cleanup tests

--- src/test/java/com/ecom/craftbid/integration/ListingControllerTest.java
+++ src/test/java/com/ecom/craftbid/integration/ListingControllerTest.java
@@ -1,12 +1,10 @@
-package com.ecom.craftbid;
+package com.ecom.craftbid.integration;
 
 import com.ecom.craftbid.dtos.ListingDTO;
 import com.ecom.craftbid.entities.listing.Listing;
-import com.ecom.craftbid.entities.listing.Tag;
 import com.ecom.craftbid.repositories.ListingRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -17,15 +15,13 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.Set;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
 @ActiveProfiles("test")
-public class ListingControllerIntegrationTest {
+public class ListingControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -34,7 +30,7 @@ public class ListingControllerIntegrationTest {
     private ListingRepository listingRepository;
     @Test
     public void testGetListingById() throws Exception {
-        long id = 11;
+        long id = 1;
 
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/public/listings/" + id))
                 .andExpect(MockMvcResultMatchers.status().isOk())
