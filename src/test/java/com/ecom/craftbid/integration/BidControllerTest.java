0738516dc704178b62e00ca9d817d87c423a75c8
Marek Szulc <235991@edu.p.lodz.pl>
2023-06-16 00:31:40 +0200

    Add test for getting all bids and bid by its id

--- src/test/java/com/ecom/craftbid/integration/BidControllerTest.java
+++ src/test/java/com/ecom/craftbid/integration/BidControllerTest.java
@@ -4,6 +4,7 @@ import com.ecom.craftbid.dtos.BidCreateRequest;
 import com.ecom.craftbid.dtos.BidDTO;
 import com.ecom.craftbid.entities.listing.Bid;
 import com.ecom.craftbid.repositories.BidRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
@@ -18,8 +19,9 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -64,4 +66,58 @@ public class BidControllerTest {
         Bid bid = bidRepository.findById(bidDTO.getId()).orElse(null);
         assertNull(bid);
     }
+
+    @Test
+    public void createAndGetBidThenAllBids() throws Exception {
+        BidDTO bidDTO = createBid(100, "test bid");
+
+        BidDTO bidGetByIdResult = new ObjectMapper().readValue(
+                mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/public/bids/" + bidDTO.getId()))
+                        .andExpect(MockMvcResultMatchers.status().isOk())
+                        .andReturn().getResponse().getContentAsString(),
+                BidDTO.class);
+
+        assertNotNull(bidGetByIdResult);
+
+        createBid(200, "test bid 2");
+        createBid(300, "test bid 3");
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/public/bids"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        String responseContent = result.getResponse().getContentAsString();
+        List<BidDTO> bidDTOList = new ObjectMapper().readValue(responseContent, new TypeReference<List<BidDTO>>() {
+        });
+
+        assertNotNull(bidDTOList);
+        final int bidsAddedByDataInit = 2;
+        assertEquals(3 + bidsAddedByDataInit, bidDTOList.size());
+        assertEquals(bidDTOList.get(2).getDescription(), "test bid");
+        for (int i = 3; i < bidDTOList.size(); i++) {
+            assertEquals(bidDTOList.get(i).getDescription(), "test bid " + (i - 1));
+        }
+    }
+
+    private BidDTO createBid(long price, String description) throws Exception {
+        BidCreateRequest bidCreateRequest1 = new BidCreateRequest();
+        bidCreateRequest1.setPrice(price);
+        bidCreateRequest1.setListingId(1);
+        bidCreateRequest1.setBidderId(1);
+        bidCreateRequest1.setDaysToDeliver(10);
+        bidCreateRequest1.setDescription(description);
+
+        String json = new ObjectMapper().writeValueAsString(bidCreateRequest1);
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/private/bids")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(json))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andReturn();
+
+        String responseContent = result.getResponse().getContentAsString();
+
+        return new ObjectMapper().readValue(responseContent, BidDTO.class);
+    }
+
 }
