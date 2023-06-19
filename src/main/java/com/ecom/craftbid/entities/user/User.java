9476a48fff78841827a67242fb84e0104b6eb7ea
Maciej Iwan <64867638+MaciejIwan@users.noreply.github.com>
2023-06-19 12:50:47 +0200

    add @JsonIgnore to user

--- src/main/java/com/ecom/craftbid/entities/user/User.java
+++ src/main/java/com/ecom/craftbid/entities/user/User.java
@@ -31,6 +31,7 @@ public class User implements UserDetails {
 
     @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "profile_id")
+    @JsonIgnore
     private Profile profile;
 
     @Enumerated(EnumType.STRING)
