730f686bb0ac8014d8ad8a06b7bcd97a463280ef
Maciej Iwan <64867638+MaciejIwan@users.noreply.github.com>
2023-12-07 20:04:09 +0100

    maly logger

--- src/main/java/com/ecom/craftbid/controllers/DemoController.java
+++ src/main/java/com/ecom/craftbid/controllers/DemoController.java
@@ -1,6 +1,8 @@
 package com.ecom.craftbid.controllers;
 
 import com.ecom.craftbid.dtos.HelloResponse;
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,9 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/api/v1/private/hello-world")
+@Log
 public class DemoController {
     @GetMapping
     public ResponseEntity<HelloResponse> helloWorld() {
+        log.info("Hello World");
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String currentUserName = authentication.getName();
         return ResponseEntity.ok(new HelloResponse("Hello from secured World: " + currentUserName + "!"));
